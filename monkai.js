09899cec6ce8e00609bca379ed3936baa69f3423
TheChadwick37 <chad.mestres@protonmail.com>
2023-03-12 00:35:11 -0500

    Added voice recognition and some responses

--- monkai.js
+++ monkai.js
@@ -1,14 +1,35 @@
 //Function for button
 
-function Run(){
-  alert("Works");
-}
-
 var vid = document.getElementById('monkey_vid');
 var lang = document.getElementById('languages');
 var sub = 0;
 var path = "Monkey_Faces/monkey_face["+sub+"]"
-var path = "file:///Users/chadmestres/MonkAI/Monkey_Faces/monkey_face["+sub+"].mp4"
+
+
+
+if (annyang) {
+  // Let's define our first command. First the text we expect, and then the function it should call
+  var commands = {
+    'hello': function() {
+      PlayVid();
+    },
+
+    'hi': function() {
+      PlayVid();
+    }
+  };
+
+  // Add our commands to annyang
+  annyang.addCommands(commands);
+
+  // Start listening. You can call this here, or attach this call to an event, button, etc.
+  annyang.start();
+}
+
+function Run(){
+  alert("Works");
+}
+
 
 
 function FilePath(select){
@@ -18,29 +39,38 @@ function FilePath(select){
   }
   if (select == "english"){
     sub = 0;
-    path = "file:///Users/chadmestres/MonkAI/Monkey_Faces/monkey_face["+sub+"].mp4"
+    path = "/Monkey_Faces/monkey_face["+sub+"].mp4"
     vid.src = path;
   }
   if (select == "french"){
     sub = 1;
-    path = "file:///Users/chadmestres/MonkAI/Monkey_Faces/monkey_face["+sub+"].mp4"
-    vid.src = path;
-  }
-  if (select == "spanish"){
-    sub = 3;
-    path = "file:///Users/chadmestres/MonkAI/Monkey_Faces/monkey_face["+sub+"].mp4"
+    path = "/Monkey_Faces/monkey_face["+sub+"].mp4"
     vid.src = path;
   }
   if (select == "japanese"){
     sub = 2;
-    path = "file:///Users/chadmestres/MonkAI/Monkey_Faces/monkey_face["+sub+"].mp4"
+    path = "/Monkey_Faces/monkey_face["+sub+"].mp4"
+    vid.src = path;
+  }
+  if (select == "spanish"){
+    sub = 3;
+    path = "/Monkey_Faces/monkey_face["+sub+"].mp4"
+    vid.src = path;
+  }
+  if (select == "korean"){
+    sub = 4;
+    path = "/Monkey_Faces/monkey_face["+sub+"].mp4"
+    vid.src = path;
+  }
+  if (select == "chinese"){
+    sub = 5;
+    path = "/Monkey_Faces/monkey_face["+sub+"].mp4"
     vid.src = path;
   }
   // alert(select);
 }
 
 function PlayVid(){
-
   //if (lang.value == "english"){
     vid.play();
 
